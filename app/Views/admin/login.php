7c1805c9b8196121d680a6e26d2f46f9b3b8c14a
Sreehari S <connect2sreehari@gmail.com>
2021-06-04 18:26:46 +0530

    Update login.php

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -29,18 +29,16 @@
         <?= $alert_msg ?>
         <form method="post" accept-charset="utf-8">
           <?= csrf_field() ?>
-          <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email" name="identity" value="<?=set_value('identity')?>">
+          <div class="input-group">
+            <input type="email" class="form-control" placeholder="Email" name="identity" value="<?= set_value('identity') ?>">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
               </div>
             </div>
           </div>
-          <div>
-            <p class="text-center"><?= $validation->showError('identity', 'error') ?></p>
-          </div>
-          <div class="input-group mb-3">
+          <div class="text-center"><?= $validation->showError('identity', 'error') ?></div>
+          <div class="input-group mt-3">
             <input type="password" class="form-control" placeholder="Password" name="password">
             <div class="input-group-append">
               <div class="input-group-text">
@@ -48,10 +46,8 @@
               </div>
             </div>
           </div>
-          <div>
-            <p class="text-center"><?= $validation->showError('password', 'error') ?></p>
-          </div>
-          <div class="row">
+          <div class="text-center"><?= $validation->showError('password', 'error') ?></div>
+          <div class="row mt-3">
             <div class="col-8">
               <div class="icheck-primary">
                 <input type="checkbox" id="remember" name="remember">
